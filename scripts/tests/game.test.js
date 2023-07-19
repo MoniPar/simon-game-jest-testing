69d251eac502d59ff5f52c3571f5489d9e4c0795
Monique Parnis <monique.parnis@gmail.com>
2023-07-19 19:50:24 +0000

    Add newGame & showScore func and tests

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
 */
 
-const { game } = require("../game");
+const { game, newGame, showScore } = require("../game");
 
 // load the index.html file into Jest's mock DOM
 beforeAll(() => {
@@ -36,5 +36,32 @@ describe("game object contains correct keys", () => {
     // check if choices key contains the relevant ids
     test("choices key contains relevant ids", () => {
         expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
+    });
+});
+
+describe("newGame works correctly", () => {
+    // set up the game state with some fake values to see if newGame func resets them
+    beforeAll(() => {
+        game.score = 42;
+        game.playerMoves = ["button1", "button3"];
+        game.currentGame = ["button1", "button4"];
+        document.getElementById("score").innerText = "42";
+        newGame();
+    });
+    // test if the score has been reset
+    test("should set the game score to zero", () => {
+        expect(game.score).toEqual(0);
+    });
+    // check if the playerMoves array has been cleared
+    test("should clear the playerMoves array", () => {
+        expect(game.playerMoves.length).toEqual(0);
+    });
+    // check if the currentGame array has been cleared
+    test("should clear the currentGame array", () => {
+        expect(game.currentGame.length).toEqual(0);
+    });
+    // check if the score element displays 0
+    test("should display 0 for the element with id of score", () => {
+        expect(document.getElementById("score").innerText).toEqual(0);
     });
 });
